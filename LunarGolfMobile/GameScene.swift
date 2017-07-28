0b4405accedf20a58f2f1f1b7e47f79ec367d7e4
jakebryon <jakebryon1995@berkeley.edu>
2017-07-28 16:06:39 -0700

    Pinch and Rotate
    the pinch and rotate gestures now work, but I can’t get the hold and
    fire features to work correctly. So the rocket fires, but not in the
    right direction.

--- LunarGolfMobile/GameScene.swift
+++ LunarGolfMobile/GameScene.swift
@@ -6,14 +6,6 @@
 //  Copyright © 2017 TwoNerdsGamesCo. All rights reserved.
 //
 
-//
-//  GameScene.swift
-//  FlappyBird
-//
-//  Created by Nate Murray on 6/2/14.
-//  Copyright (c) 2014 Fullstack.io. All rights reserved.
-//
-
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate{
@@ -24,8 +16,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     var blackhole:SKSpriteNode!
     var spaceColor:SKColor!
     var canRestart = Bool()
+    var RocketFire = Bool(false)
     var scoreLabelNode:SKLabelNode!
     var score = NSInteger()
+    var powerAmount = CGFloat(10)
+    
+    let rotateRec = UIRotationGestureRecognizer()
+    let pinchRec = UIPinchGestureRecognizer()
+    let holdRec = UILongPressGestureRecognizer()
     
     let rocketCategory: UInt32 = 1 << 0
     let worldCategory: UInt32 = 1 << 1
@@ -35,16 +33,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     override func didMove(to view: SKView) {
         
+        rotateRec.addTarget(self, action: #selector(GameScene.rotateRocket(_:)))
+        pinchRec.addTarget(self, action: #selector(GameScene.pinchGesture(_:)))
+        holdRec.addTarget(self, action: #selector(GameScene.launchRocket(_:)))
+        
         canRestart = false
-        
-        // setup physics
-        //note I am note sure how to get the contact delagate to really work
-        self.physicsWorld.gravity = CGVector( dx: 0.0, dy: 0.0 )
-        self.physicsWorld.contactDelegate = self
-        
-        // setup background color
-        spaceColor = SKColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
-        self.backgroundColor = spaceColor
         
         // setup rocket and planet
         let planetTexture = SKTexture(imageNamed: "planet")
@@ -52,71 +45,93 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         planet.setScale(0.15)
         planet.position = CGPoint(x: self.frame.size.width * 0.5, y:self.frame.size.height * 0.5)
         
-        planet.physicsBody = SKPhysicsBody(circleOfRadius: planet.size.height / 2.0)
-        planet.physicsBody?.isDynamic = true
-        planet.physicsBody?.allowsRotation = true
-        planet.physicsBody?.mass = 10000
-        planet.physicsBody?.affectedByGravity = true
-        // add gravity to planet
-        let gravity = SKFieldNode.radialGravityField()
-        gravity.strength = 15
-        gravity.categoryBitMask = gravityCategory
-        planet.addChild(gravity)
         
         let rocketTexture = SKTexture(imageNamed: "rocket")
         rocket = SKSpriteNode(texture: rocketTexture)
         rocket.setScale(0.15)
         rocket.position = CGPoint(x: planet.position.x + 1.1*planet.size.width/2 + rocket.size.width/2, y:planet.position.y)
         rocket.zRotation = CGFloat(-1*Double.pi/2)
+    
         
-        rocket.physicsBody = SKPhysicsBody(circleOfRadius: rocket.size.height / 2.0)
-        rocket.physicsBody?.isDynamic = true
-        rocket.physicsBody?.allowsRotation = true
-        rocket.physicsBody?.mass = 1
-        rocket.physicsBody?.affectedByGravity = true
-        rocket.physicsBody?.restitution = 10
-        rocket.physicsBody?.linearDamping = 0
-        
-        rocket.physicsBody?.categoryBitMask = rocketCategory
-        rocket.physicsBody?.collisionBitMask = worldCategory | planetCategory
-        rocket.physicsBody?.contactTestBitMask = worldCategory | planetCategory
+        // setup background color
+        spaceColor = SKColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        self.backgroundColor = spaceColor
         
         self.addChild(planet)
         self.addChild(rocket)
         
-        
+        self.view!.addGestureRecognizer(rotateRec)
+        self.view!.addGestureRecognizer(pinchRec)
+        self.view!.addGestureRecognizer(holdRec)
         
         // create score tracker
         score = 0
         scoreLabelNode = SKLabelNode(fontNamed:"Arial")
         scoreLabelNode.position = CGPoint( x: self.frame.midX, y: 3 * self.frame.size.height / 4 )
         scoreLabelNode.zPosition = 100
-        scoreLabelNode.text = String(score)
+        scoreLabelNode.text = String(describing: powerAmount)
         self.addChild(scoreLabelNode)
+        
         
     }
     
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for _ in touches { // do we need all touches?
-            rocket.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-            rocket.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 50))
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for _ in touches { // do we need all touches?
+////            rocket.physicsBody?.applyImpulse(CGVector(dx: 10*sin(rocket.zRotation), dy: 10*cos(rocket.zRotation)))
+//        }
+//    }
+    
+    //figure out different gestures
+
+    func pinchGesture(_ sender: UIPinchGestureRecognizer){
+        if (sender.state == .changed){
+            rocket.position = CGPoint(x: planet.position.x + sin(sender.scale)*(1.1*planet.size.width/2 + rocket.size.width/2), y:planet.position.y + cos(sender.scale)*(1.1*planet.size.width/2 + rocket.size.width/2))
         }
     }
-    //figure out different gestures
-    func rotateGesture(sender: UIRotationGestureRecognizer){
-        sender.view?.transform = (sender.view?.transform)!.rotated(by: sender.rotation)
-        sender.rotation = 0
-        print("rotate gesture")
+    
+    func rotateRocket(_ sender:UIRotationGestureRecognizer){
+        if (sender.state == .changed){
+            print("rotation = ")
+            print(rocket.zRotation)
+            rocket.zRotation = -sender.rotation + CGFloat(-1*Double.pi/2)
+        }
     }
-    func pinchGesture(sender: UIPinchGestureRecognizer){
-        sender.view?.transform = (sender.view?.transform)!.scaledBy(x: sender.scale, y: sender.scale)
-        sender.scale = 1
-        print("pinch gesture")
+    
+    func launchRocket(_ sender: UILongPressGestureRecognizer) {
+        if (sender.state == .began) {
+        }
+        if (sender.state == .changed) {
+            powerAmount += 1
+            if (powerAmount > 100) {
+                powerAmount = 0
+            }
+            scoreLabelNode.text = String(describing: powerAmount)
+        }
+        if (sender.state == .ended) {
+            print("fire!")
+            RocketFire = true
+            
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // put stuff here
+        if RocketFire {
+            // setup physics
+            //note I am note sure how to get the contact delagate to really work
+            self.physicsWorld.gravity = CGVector( dx: 0.0, dy: 0.0 )
+            self.physicsWorld.contactDelegate = self
+            rocket.physicsBody = SKPhysicsBody(circleOfRadius: rocket.size.height / 2.0)
+            
+            // add gravity to planet
+            let gravity = SKFieldNode.radialGravityField()
+            gravity.strength = 5
+            gravity.categoryBitMask = gravityCategory
+            planet.addChild(gravity)
+            
+            rocket.physicsBody?.applyImpulse(CGVector(dx: -powerAmount*sin(rocket.zRotation), dy: -powerAmount*cos(rocket.zRotation)))
+        }
+        
     }
     
 }
